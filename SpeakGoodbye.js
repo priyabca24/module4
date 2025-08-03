687a03460e47ee8257c11f8d1b678f55588ff4c8
priyabca24 <priyakumari.bca.24@sittechno.edu.in>
2025-08-03 12:48:13 +0530

    Update SpeakGoodbye.js

--- SpeakGoodbye.js
+++ SpeakGoodbye.js
@@ -1,18 +1,11 @@
-// STEP 6: Wrap in an IIFE
 (function (window) {
-
-  // STEP 7: Create the object
   var byeSpeaker = {};
 
-  // DO NOT attach the speakWord to byeSpeaker
-  var speakWord = "Good Bye";
+  var farewell = "See ya";
 
-  // STEP 8: Attach speak function to byeSpeaker object
-  byeSpeaker.speak = function (name) {
-    console.log(speakWord + " " + name);
+  byeSpeaker.sayBye = function (name) {
+    console.log(farewell + ", " + name + "!");
   };
 
-  // STEP 9: Expose byeSpeaker to the global scope
   window.byeSpeaker = byeSpeaker;
-
 })(window);
