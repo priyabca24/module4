88b856ca656e95eb59c4570e8406a43a0dbafb2d
priyabca24 <priyakumari.bca.24@sittechno.edu.in>
2025-08-03 12:42:47 +0530

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,18 +1,18 @@
-// STEP 2: Wrap in an IIFE
+
 (function (window) {
 
-  // STEP 3: Create the object
+  
   var helloSpeaker = {};
 
-  // DO NOT attach the speakWord to helloSpeaker
+ 
   var speakWord = "Hello";
 
-  // STEP 4: Attach speak function to helloSpeaker object
+
   helloSpeaker.speak = function (name) {
     console.log(speakWord + " " + name);
   };
 
-  // STEP 5: Expose helloSpeaker to the global scope
+ 
   window.helloSpeaker = helloSpeaker;
 
 })(window);
